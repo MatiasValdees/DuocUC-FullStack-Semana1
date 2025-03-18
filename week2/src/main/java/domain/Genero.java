02e03bdb380697f8d55ce53b01467ce8dce98160
MatiasValdees <matias.valdesr3@gmail.com>
2025-03-18 17:03:50 -0300

    Crear clases POJO

---
+++ week2/src/main/java/domain/Genero.java
@@ -0,0 +1,17 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Genero {
+    private int id;
+    private String nombre;
+}
